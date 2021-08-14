d11bd50b356d0c705b35a60ee9a7a2c820e1ade6
tolumide-ng <tolumideshopein@gmail.com>
2021-08-14 19:18:09 +0100

    removes unused route

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
