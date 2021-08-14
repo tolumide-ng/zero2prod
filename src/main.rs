50869b936dc8ce0933f63ce86397adda8977ad2c
tolumide-ng <tolumideshopein@gmail.com>
2021-08-14 04:27:59 +0100

    creates health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Import the code I want to test
+    use super::*;
+
+    // 
 }
