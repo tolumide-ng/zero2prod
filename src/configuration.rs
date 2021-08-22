04def90ca969c1149a7d11a073d9b8a0177d507d
tolumide-ng <tolumideshopein@gmail.com>
2021-08-22 10:47:35 +0100

    implements connection string method on databaseSettings struct

--- src/configuration.rs
+++ src/configuration.rs
@@ -14,14 +14,13 @@ pub struct DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    // Initialise our configuration reader
     let mut settings = config::Config::default();
-
-    // Add configuration values from a file named 'configuration'.
-    // It will look for any top-level file with an extension
-    // that `config` knows how to parse: yaml, json, etc.
-
     settings.merge(config::File::with_name("configuration"))?;
-    // try to convert the configuration values it read into our settings type
     settings.try_into()
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        return format!("postgres://{}:{}@{}:{}/{}", self.username, self.password, self.host, self.port, self.database_name);
+    }
 }
