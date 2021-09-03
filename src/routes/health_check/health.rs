bd3c50b14fbce710076b391930e27ce68725a761
tolumide-ng <tolumideshopein@gmail.com>
2021-09-03 02:11:47 +0100

    assigns value to the id and subscribed_at column

--- src/routes/health_check/health.rs
+++ src/routes/health_check/health.rs
@@ -1,6 +1,5 @@
 use crate::routes::prelude::*;
 
 pub async fn health_check() -> HttpResponse {
-    println!("received a request here");
     HttpResponse::Ok().finish()
 }
--- src/routes/subscriptions/subscribe.rs
+++ src/routes/subscriptions/subscribe.rs
@@ -1,7 +1,8 @@
 use crate::routes::prelude::*;
 use sqlx::PgPool;
-use log;
 use uuid::Uuid;
+use tracing;
+use chrono::Utc;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -12,23 +13,25 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
 
-    log::info!("Correlation Id: {} - Adding {} {} as a new subscriber", request_id, form.name, form.email);
-    log::info!("Correlation Id: {} - Saving new subscriber's details in the database", request_id);
+    tracing::info!("Correlation Id: {} - Adding {} {} as a new subscriber", request_id, form.name, form.email);
+    tracing::info!("Correlation Id: {} - Saving new subscriber's details in the database", request_id);
 
     match sqlx::query!(
         r#"
-        INSERT INTO subscriptions (email, name) 
-        VALUES ($1, $2)
+        INSERT INTO subscriptions (id, email, name, subscribed_at) 
+        VALUES ($1, $2, $3, $4)
         "#,
+        Uuid::new_v4(),
         form.email,
-        form.name
+        form.name,
+        Utc::now()
     ).execute(pool.get_ref()).await {
         Ok(_) => {
-            log::info!("Correlation Id: {} - New subscriber detail saved", request_id);
+            tracing::info!("Correlation Id: {} - New subscriber detail saved", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("Correlation Id: {} - Failed to execute query {:#?}", request_id, e);
+            tracing::error!("Correlation Id: {} - Failed to execute query {:#?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
