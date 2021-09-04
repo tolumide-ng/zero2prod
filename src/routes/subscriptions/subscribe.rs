8e9c7c24089d3638614c371174b081611c5dcd26
tolumide-ng <tolumideshopein@gmail.com>
2021-09-04 22:53:08 +0100

    adds insert_subscriber route

--- src/routes/subscriptions/subscribe.rs
+++ src/routes/subscriptions/subscribe.rs
@@ -11,16 +11,18 @@ pub struct FormData {
     pub name: String,
 }
 
+
+#[tracing::instrument (
+    name = "Adding a new subscriber"
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-
-    // tracing::info!("Correlation Id: {} - Adding {} {} as a new subscriber", request_id, form.name, form.email);
-    // tracing::info!("Correlation Id: {} - Saving new subscriber's details in the database", request_id);
-
-    let request_span = tracing::info_span!("Adding a new subscriber", %request_id, subscriber_email=%form.email, subscriber_name=%form.name);
-
-    let _request_span_guard = request_span.enter();
-
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
     match sqlx::query!(
@@ -41,5 +43,22 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
             HttpResponse::InternalServerError().finish()
         }
     }
+}
+
+
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: & PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+    "#, Uuid::new_v4(), form.email, form.name, Utc::now()).execute(pool).await.map_err(|e| {
+        tracing::error!("Failed to execute query {:?}", e)
+    })?;
+
+    Ok(())
 }
 
