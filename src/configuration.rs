534f1f67cd3b7719e97223bb5e742763b6c0b3eb
tolumide-ng <tolumideshopein@gmail.com>
2021-09-16 02:00:05 +0100

    rearranges src/configuration and adds serde-aux

--- src/configuration.rs
+++ src/configuration.rs
@@ -17,6 +17,7 @@ pub enum Environment {
     Production
 }
 
+
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
@@ -39,6 +40,31 @@ impl DatabaseSettings {
 }
 
 
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production"
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environement. Use either `local` or `production`.", other
+            ))
+        }
+    }
+}
+
+
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
 
@@ -58,28 +84,4 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     settings.merge(config::Environment::with_prefix("app").separator("__"))?;
 
     settings.try_into()
-}
-
-impl Environment {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            Environment::Local => "local",
-            Environment::Production => "production"
-        }
-    }
-}
-
-
-impl TryFrom<String> for Environment {
-    type Error = String;
-
-    fn try_from(s: String) -> Result<Self, Self::Error> {
-        match s.to_lowercase().as_str() {
-            "local" => Ok(Self::Local),
-            "production" => Ok(Self::Production),
-            other => Err(format!(
-                "{} is not a supported environement. Use either `local` or `production`.", other
-            ))
-        }
-    }
 }
