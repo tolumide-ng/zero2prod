5bd06053f32de3c052020471097fb794dff4e048
tolumide-ng <tolumideshopein@gmail.com>
2021-09-17 01:37:33 +0100

    updates configuration (digital ocean) and adds ssl to db options

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,6 @@
 use std::convert::{TryInto, TryFrom};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
@@ -29,12 +29,20 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
 
     pub fn without_db(&self) -> PgConnectOptions {
-        PgConnectOptions::new().host(&self.host).username(&self.username).password(&self.password).port(self.port)
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
+
+        PgConnectOptions::new().host(&self.host).username(&self.username).password(&self.password).port(self.port).ssl_mode(ssl_mode)
     }
 
     
