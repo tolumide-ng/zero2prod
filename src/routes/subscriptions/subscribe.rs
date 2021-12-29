9ee3ef2685da92f14eca2fd65724b9783360b553
tolumide-ng <tolumideshopein@gmail.com>
2021-12-29 15:07:14 +0100

    user name validation

--- src/routes/subscriptions/subscribe.rs
+++ src/routes/subscriptions/subscribe.rs
@@ -1,8 +1,9 @@
 use crate::routes::prelude::*;
+use chrono::Utc;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 use tracing;
-use chrono::Utc;
 
 
 #[derive(serde::Deserialize)]
@@ -21,6 +22,10 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish()
+    }
 
     match insert_subscriber(&pool, &form).await {
         Ok(_) => {
@@ -59,3 +64,13 @@ pub async fn insert_subscriber(pool: & PgPool, form: &FormData) -> Result<(), sq
     Ok(())
 }
 
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+
+   !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
+}
