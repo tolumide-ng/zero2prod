69b9a6cf8491ff987880c98ca3f1421e4f71fdd4
tolumide-ng <tolumideshopein@gmail.com>
2021-12-31 00:09:37 +0100

    Creates request body on EmailClient.send_email

--- src/email/email_client.rs
+++ src/email/email_client.rs
@@ -1,6 +1,15 @@
 use reqwest::Client;
 use crate::domain::subscriber_email::SubscriberEmail;
 
+#[derive(serde::Serialize)]
+struct SendEmailRequest {
+    from: String,
+    to: String,
+    subject: String,
+    html_body: String,
+    text_body: String,
+}
+
 pub struct EmailClient {
     sender: SubscriberEmail,
     http_client: Client,
@@ -8,8 +17,26 @@ pub struct EmailClient {
 }
 
 impl EmailClient {
-    pub async fn send_email(&self, recipient: SubscriberEmail, subject: &str, html_content: &str, text_content: &str) -> Result<(), String> {
-        todo!()
+    pub async fn send_email(
+        &self, 
+        recipient: SubscriberEmail, 
+        subject: &str, 
+        html_content: &str, 
+        text_content: &str
+    ) -> Result<(), String> {
+        let url = format!("{}/email", &self.base_url);
+
+        let request_body = SendEmailRequest {
+            from: self.sender.as_ref().to_owned(),
+            to: recipient.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            html_body: html_content.to_owned(),
+            text_body: text_content.to_owned(),
+        };
+        let builder = self.http_client.post(&url).json(&request_body);
+        
+        Ok(())
+
     }
 
     pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
@@ -18,5 +45,34 @@ impl EmailClient {
             base_url,
             sender
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::email::email_client::EmailClient;
+    use crate::domain::subscriber_email::SubscriberEmail;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::{Faker, Fake};
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any()).respond_with(ResponseTemplate::new(200)).expect(1).mount(&mock_server).await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        let _ = email_client.send_email(subscriber_email, &subject, &content, &content).await;
     }
 }
