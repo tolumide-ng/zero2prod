2aaf34bae840a3c9ffa14fcf46946a5af29c8dc9
tolumide-ng <tolumideshopein@gmail.com>
2021-12-31 17:31:54 +0100

    Adds status column to subscriptions table

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,4 +1,4 @@
-use crate::helpers::{spawn_app, TestApp};
+use crate::helpers::{spawn_app};
 
 
 
@@ -30,7 +30,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=sample", "missing the email"),
         ("email=sample%40email.com", "missing the name"),
@@ -40,7 +39,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     for (invalid_body, error_message) in test_cases {
         // Act 
 
-        let response = app.post_subscription(body.to_string()).await;
+        let response = app.post_subscription(invalid_body.to_string()).await;
 
         // Assert
         assert_eq!(400, response.status().as_u16(), 
@@ -54,7 +53,6 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 #[actix_rt::test]
 async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
         ("name=Ursula&email=", "empty email"),
