5ba499174c6924aba272940c26ad7f0d00b688cc
tolumide-ng <tolumideshopein@gmail.com>
2021-12-31 18:02:49 +0100

    Migration to create subscription_tokens table

--- migrations/20211231163554_make_status_not_null_in_subscriptions.sql
+++ migrations/20211231163554_make_status_not_null_in_subscriptions.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
 -- We wrap the whole migration in a transaction to make sure it succeeds or fails atomically.
 -- 'sqlx` does not do it automatically for us.
-BEGIN:
+BEGIN;
     -- Backfill `status` for historical entries
     UPDATE subscriptions
         SET status = 'confirmed'
---
+++ migrations/20211231165728_create_subscription_tokens_table.sql
@@ -0,0 +1,7 @@
+-- Add migration script here
+CREATE TABLE subscription_tokens(
+    subscription_token uuid NOT NULL,
+    subscriber_id uuid NOT NULL
+        REFERENCES subscriptions (id),
+    PRIMARY KEY (subscription_token)
+);
