c59b78da8063bd8308741c91c3a895ebf415e115
tolumide-ng <tolumideshopein@gmail.com>
2021-12-31 03:04:09 +0100

    handles error responses from the email server

--- src/email/email_client.rs
+++ src/email/email_client.rs
@@ -43,7 +43,8 @@ impl EmailClient {
             .header("X-Postmark-Server-Token", &self.authorization_token)
             .json(&request_body)
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
         
         Ok(())
 
@@ -74,7 +75,7 @@ mod tests {
     use wiremock::matchers::{any, header, header_exists, path, method};
     use wiremock::{Mock, MockServer, ResponseTemplate};
     use wiremock::Request;
-    use claim::assert_ok;
+    use claim::{assert_ok, assert_err};
 
     struct SendEmailBodyMatcher;
 
@@ -140,5 +141,27 @@ mod tests {
         let outcome = email_client.send_email(subscriber_email, &subject, &content, &content).await;
 
         assert_ok!(outcome);
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_the_server_returns_500() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Faker.fake());
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+
+        let outcome = email_client.send_email(subscriber_email, &subject, &content, &content).await;
+
+        assert_err!(outcome);
     }
 }
