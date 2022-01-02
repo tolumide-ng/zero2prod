753a65801fab13fa55571a18ba6a91b8a3b995e0
tolumide-ng <tolumideshopein@gmail.com>
2022-01-02 22:07:13 +0100

    Test for email newsletter endpoint

--- tests/api/helpers/email.rs
+++ tests/api/helpers/email.rs
@@ -1,4 +1,26 @@
+use wiremock::matchers::{any, method, path};
+use wiremock::{Mock, ResponseTemplate};
+
+use super::app::TestApp;
+
 pub struct ConfirmationLinks {
     pub html: reqwest::Url,
     pub plain_text: reqwest::Url,
+}
+
+pub async fn create_unconfirmed_subcriber(app: &TestApp) {
+    let body = "name=le%20guin&email=ursulua_le_guin%40gmail.com";
+
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .named("Create unconfirmed subscriber")
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscription(body.into())
+        .await
+        .error_for_status()
+        .unwrap();
 }
---
+++ tests/api/newsletters.rs
@@ -0,0 +1,34 @@
+use crate::helpers::app::{spawn_app, TestApp};
+use wiremock::matchers::{any, method, path};
+use wiremock::{Mock, ResponseTemplate};
+use crate::helpers::email::create_unconfirmed_subcriber;
+
+
+#[actix_rt::test]
+async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
+    let app = spawn_app().await;
+    create_unconfirmed_subcriber(&app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        .expect(0)
+        .mount(&app.email_server)
+        .await;
+
+    
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</P>",
+        }
+    });
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await.expect("Failed to execute request");
+
+    assert_eq!(response.status().as_u16(), 200);
+}
