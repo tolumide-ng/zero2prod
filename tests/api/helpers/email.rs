504ae6cbbc69abe1db8f3bb90516a1636cbfcc47
tolumide-ng <tolumideshopein@gmail.com>
2022-01-03 00:21:57 +0100

    Adds test to confirm newsletter was sent and updates /nwsletters endpoint from a 'get' request to a 'post'

--- tests/api/helpers/email.rs
+++ tests/api/helpers/email.rs
@@ -8,7 +8,7 @@ pub struct ConfirmationLinks {
     pub plain_text: reqwest::Url,
 }
 
-pub async fn create_unconfirmed_subcriber(app: &TestApp) {
+pub async fn create_unconfirmed_subcriber(app: &TestApp) -> ConfirmationLinks {
     let body = "name=le%20guin&email=ursulua_le_guin%40gmail.com";
 
     let _mock_guard = Mock::given(path("/email"))
@@ -21,6 +21,26 @@ pub async fn create_unconfirmed_subcriber(app: &TestApp) {
 
     app.post_subscription(body.into())
         .await
+        .error_for_status()
+        .unwrap();
+
+    let email_request = &app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+
+    app.get_confirmation_links(&email_request)
+}
+
+
+pub async fn create_confirmed_subscriber(app: &TestApp) {
+    let confirmation_link = create_unconfirmed_subcriber(app).await;
+    reqwest::get(confirmation_link.html)
+        .await
+        .unwrap()
         .error_for_status()
         .unwrap();
 }
--- tests/api/newsletters.rs
+++ tests/api/newsletters.rs
@@ -1,7 +1,7 @@
 use crate::helpers::app::{spawn_app, TestApp};
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
-use crate::helpers::email::create_unconfirmed_subcriber;
+use crate::helpers::email::{create_confirmed_subscriber, create_unconfirmed_subcriber};
 
 
 #[actix_rt::test]
@@ -31,4 +31,35 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
         .await.expect("Failed to execute request");
 
     assert_eq!(response.status().as_u16(), 200);
+}
+
+
+#[actix_rt::test]
+async fn newsletters_are_delivered_to_confirmed_subscribers() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>"
+        }
+    });
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 200)
 }
