b476a3d5d984de3031ffc466c6c062a3c77fe94c
tolumide-ng <tolumideshopein@gmail.com>
2022-01-02 04:57:10 +0100

    Error traits implementation for StoreTokenError

--- src/routes/subscriptions/error.rs
+++ src/routes/subscriptions/error.rs
@@ -1,8 +1,6 @@
 use actix_web::ResponseError;
 
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
-
 
 impl std::fmt::Display for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -14,5 +12,33 @@ impl std::fmt::Display for StoreTokenError {
     }
 }
 
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
 
 impl ResponseError for StoreTokenError {}
+
+
+impl std::error::Error for StoreTokenError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+
+fn error_chain_fmt(
+    e: &impl std::error::Error, 
+    f: &mut std::fmt::Formatter<'_>
+) -> std::fmt::Result {
+    write!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        write!(f, "Caused by \n\t{}", cause)?;
+        current = cause.source();
+    }
+
+    Ok(())
+}
