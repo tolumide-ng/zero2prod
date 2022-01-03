7f4cc72775730d736890c9153453b7e866a9bd48
tolumide-ng <tolumideshopein@gmail.com>
2022-01-03 21:16:02 +0100

    Request body for /newsletter endpoint

--- src/routes/newsletter/route.rs
+++ src/routes/newsletter/route.rs
@@ -1,6 +1,19 @@
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, web};
+use serde::Deserialize;
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
 
 
-pub async fn publish_newsletter() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content
+}
+
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
