72206ed491c2d51be99d24248c42b51bc4731741
tolumide-ng <tolumideshopein@gmail.com>
2022-01-03 21:26:29 +0100

    User gets a 400 response when they send an invalid request body to the /newsletters endpoint

--- tests/api/newsletters.rs
+++ tests/api/newsletters.rs
@@ -63,3 +63,37 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
 
     assert_eq!(response.status().as_u16(), 200)
 }
+
+
+#[actix_rt::test]
+async fn newsletters_returns_400_for_invalid_data() {
+    let app = spawn_app().await;
+    let test_cases = vec![
+        (
+            serde_json::json!({
+                "content": {
+                    "text": "Newsletter body as plain text",
+                    "html": "<p>Newsletter body as HTML</p>",
+                }
+            }),
+            "missing title",
+        )
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = reqwest::Client::new()
+            .post(&format!("{}/newsletters", &app.address))
+            .json(&invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
+}
