92456eb428d5524ad1de6dd0a8d6bbca9aa4630b
tolumide-ng <tolumideshopein@gmail.com>
2022-01-04 22:19:51 +0100

    Remove unnecessary struct on get_confirmed_subscribers

--- src/routes/newsletter/route.rs
+++ src/routes/newsletter/route.rs
@@ -50,21 +50,14 @@ impl ResponseError for PublishError {
 )]
 async fn get_confirmed_subscribers(pool: &PgPool) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
 
-    struct Row {
-        email: String
-    }
-
-    let rows = sqlx::query_as!(
-        Row,
+    let confirmed_subscribers = sqlx::query!(
         r#"
         SELECT email 
         FROM subscriptions 
         WHERE status = 'confirmed'
         "#,
     ).fetch_all(pool)
-        .await?;
-
-    let confirmed_subscribers = rows
+        .await?
         .into_iter()
         .map(|r| match SubscriberEmail::parse(r.email) {
             Ok(email) => Ok(ConfirmedSubscriber {email}),
