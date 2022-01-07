ab1f78eb3178e4ba2df4f1cea3f504cf107d2672
tolumide-ng <tolumideshopein@gmail.com>
2022-01-07 21:47:13 +0100

    Saves sha_256 hash as user's password

--- src/routes/auth/helper.rs
+++ src/routes/auth/helper.rs
@@ -3,19 +3,22 @@ use sqlx::{PgPool};
 use anyhow::Context;
 use crate::helpers::authentication::Credentials;
 use crate::errors::publish_error::PublishError;
+use sha3::Digest;
 
 pub async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool
 ) -> Result<uuid::Uuid, PublishError> {
+    let hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    let hash = format!("{:x}", hash);
     let user_id: Option<_> = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND hash = $2
         "#,
         credentials.username,
-        credentials.password.expose_secret()
+        hash
     )
     .fetch_optional(pool)
     .await
