01bcffe33605276b3e156af0d9ae3204119f798f
tolumide-ng <tolumideshopein@gmail.com>
2022-01-08 04:55:13 +0100

    Break down validate_credentials function into separate functions

--- src/routes/auth/validate_credentials.rs
+++ src/routes/auth/validate_credentials.rs
@@ -2,7 +2,8 @@ use anyhow::Context;
 use pbkdf2::password_hash::{PasswordVerifier, PasswordHash};
 use pbkdf2::Pbkdf2;
 use secrecy::ExposeSecret;
-use sqlx::{PgPool};
+use sqlx::PgPool;
+use secrecy::Secret;
 
 use crate::helpers::authentication::Credentials;
 use crate::errors::publish_error::PublishError;
@@ -14,37 +15,12 @@ pub async fn validate_credentials(
     pool: &PgPool
 ) -> Result<uuid::Uuid, PublishError> {
 
-    // let salt = SaltString::generate(&mut OsRng);
-    // let phc_hash = Pbkdf2.hash_password(&[39], &salt).unwrap().to_string();
-    // let parsed_hash = PasswordHash::new(&phc_hash)
-    //     .context("Failed to parse the error").map_err(PublishError::UnexpectedError)?;
-
     let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, &pool)
         .await
-        .map_err(PublishError::UnexpectedError)
+        .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
 
-    let row: Option<_> = sqlx::query!(
-        r#"
-        SELECT user_id, hash
-        FROM users
-        WHERE username = $1
-        "#,
-        credentials.username,
-    )
-    .fetch_optional(pool)
-    .await
-    .context("Failed to perform a query to retrieve stored credentials.")
-    .map_err(PublishError::UnexpectedError)?;
-
-    let (expected_password_hash, user_id) = match row {
-        Some(row) => (row.hash, row.user_id),
-        None => {
-            return Err(PublishError::AuthError(anyhow::anyhow!("Unknown username")))
-        }
-    };
-
-    let expected_password_hash = PasswordHash::new(&expected_password_hash)
+    let expected_password_hash = PasswordHash::new(&expected_password_hash.expose_secret())
         .context("Failed to parse hash in PHC string format.")
         .map_err(PublishError::UnexpectedError)?;
 
@@ -57,3 +33,23 @@ pub async fn validate_credentials(
 }
 
 
+#[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
+async fn get_stored_credentials(
+    username: &str,
+    pool: &PgPool
+) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+        SELECT user_id, hash
+        FROM users
+        WHERE username = $1
+        "#,
+        username
+    )
+        .fetch_optional(pool)
+        .await
+        .context("Failed to perform a query to retrieve stored credentials.")?
+        .map(|row| (row.user_id, Secret::new(row.hash)));
+
+        Ok(row)
+}
