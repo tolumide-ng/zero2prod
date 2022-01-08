dbd26a837cbf55226d2ac2ecf0047e9358e8f94f
tolumide-ng <tolumideshopein@gmail.com>
2022-01-08 03:10:59 +0100

    Changes argon2 to pbkdf2 owing to cargo compilation issues with current version of argon2

--- src/routes/auth/helper.rs
+++ src/routes/auth/helper.rs
@@ -1,8 +1,8 @@
 use argon2::{Algorithm, Argon2, Version, Params};
 use anyhow::Context;
 use secrecy::ExposeSecret;
-// use sha3::Digest;
 use sqlx::{PgPool};
+use argon2::PasswordHasher;
 
 use crate::helpers::authentication::Credentials;
 use crate::errors::publish_error::PublishError;
@@ -12,33 +12,46 @@ pub async fn validate_credentials(
     pool: &PgPool
 ) -> Result<uuid::Uuid, PublishError> {
     // let hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
-    let hash = Argon2::new(
+    let hasher = Argon2::new(
         Algorithm::Argon2id,
         Version::V0x13,
         Params::new(15000, 2,1, None)
             .context("Failed to build Argon2 parameters")
             .map_err(PublishError::UnexpectedError)?,
     );
-    
-    let hash = format!("{:x}", hash);
-    let user_id: Option<_> = sqlx::query!(
+
+    let row: Option<_> = sqlx::query!(
         r#"
-        SELECT user_id
+        SELECT user_id, hash, salt
         FROM users
-        WHERE username = $1 AND hash = $2
+        WHERE username = $1
         "#,
         credentials.username,
-        hash
     )
     .fetch_optional(pool)
     .await
-    .context("Failed to perform a query to validate auth credentials.")
+    .context("Failed to perform a query to retrieve stored credentials.")
     .map_err(PublishError::UnexpectedError)?;
 
-    user_id
-        .map(|row| row.user_id)
-        .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
-        .map_err(PublishError::AuthError)
+    let (expected_password_hash, user_id, salt) = match row {
+        Some(row) => (row.hash, row.id, row.salt),
+        None => {
+            return Err(PublishError::AuthError(anyhow::anyhow!("Unknown username")))
+        }
+    };
+
+    let hash = hasher
+        .hash_password(credentials.password.expose_secret().as_bytes(), &salt)
+        .context("Failed to hash password")
+        .map_err(PublishError::UnexpectedError)?;
+
+    let hash = format!(":x", hash.hash.unwrap());
+
+    if hash != expected_password_hash {
+        Err(PublishError::AuthError(anyhow::anyhow!("Invalid password.")))
+    } else {
+        Ok(user_id)
+    }
 }
 
 
