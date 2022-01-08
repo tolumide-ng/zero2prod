4890488c2fa1a99d7d239e2457cce20a5883cb1e
tolumide-ng <tolumideshopein@gmail.com>
2022-01-08 04:28:22 +0100

    Update test user to use pbkdf2 for passwords

--- tests/api/helpers/user.rs
+++ tests/api/helpers/user.rs
@@ -1,6 +1,11 @@
-use sha3::Digest;
+use anyhow::Context;
+use pbkdf2::{password_hash::{
+    rand_core::OsRng,
+    PasswordHash, PasswordHasher, SaltString
+}, Pbkdf2};
 use uuid::Uuid;
 use sqlx::PgPool;
+use zero2prod::errors::publish_error::PublishError;
 
 pub struct TestUser {
     pub user_id: Option<Uuid>,
@@ -18,13 +23,22 @@ impl TestUser {
     }
 
     pub async fn store(&mut self, pool: &PgPool) {
-        let hash = sha3::Sha3_256::digest(self.password.as_bytes());
-        let hash = format!("{:x}", hash);
+        // let hash = sha3::Sha3_256::digest(self.password.as_bytes());
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = Pbkdf2
+            .hash_password(self.password.as_bytes(), &salt)
+            .context("Failed to hash password").map_err(PublishError::UnexpectedError).unwrap().to_string();
+
+        // let hash = format!("{:x}", hash);
+        let parsed_hash = PasswordHash::new(&password_hash)
+            .context("Error parsing hash")
+            .map_err(PublishError::UnexpectedError).unwrap().to_string();
+
         let user = sqlx::query!(
             "INSERT INTO users (username, hash)
             VALUES ($1, $2) RETURNING user_id",
             self.username,
-            hash
+            parsed_hash
         )
             .fetch_one(pool)
             .await
