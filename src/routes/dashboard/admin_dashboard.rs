7101cc530d9ec5f348807732624bbb13e1842152
tolumide-ng <tolumideshopein@gmail.com>
2022-01-09 23:59:10 +0100

    Adds e500 on dashboard route

--- src/routes/dashboard/admin_dashboard.rs
+++ src/routes/dashboard/admin_dashboard.rs
@@ -1,4 +1,21 @@
-use actix_web::HttpResponse;
-pub async fn admin_dashboard() -> HttpResponse {
-    HttpResponse::Ok().finish()
+use actix_web::{web, HttpResponse};
+use actix_session::Session;
+
+fn e500<T>(e: T) -> actix_web::error::InternalError<T> {
+    actix_web::error::InternalError::from_response(e, HttpResponse::InternalServerError().finish())
+}
+
+
+pub async fn admin_dashboard(
+    session: Session,
+) -> Result<HttpResponse, actix_web::Error> {
+    let username = if let Some(user_id) = session.get::<Uuid>("user_id")
+        .map_err(e500)? 
+    {
+        todo!()
+    } else {
+        todo!()
+    }
+
+    Ok(HttpResponse::Ok().finish())
 }
