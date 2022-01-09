89088d70f65ca22bccc7f2613c7b086dfbc7bc0b
tolumide-ng <tolumideshopein@gmail.com>
2022-01-09 17:57:06 +0100

    Updates test

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -11,21 +11,22 @@ async fn an_error_flash_message_is_set_on_failure() {
         "username": "random-username",
         "password": "random-password"
     });
+    
     let response = app.post_login(&login_body).await;
-
-    // Part 2 - Follow the redirect
     let flash_cookies = response.cookies().find(|c| c.name() == "_flash").unwrap();
-
     assert_eq!(flash_cookies.valu(), "Authentication failed");
-
+    
     assert_eq!(response.status().as_u16(), 303);
     assert_eq!(response.headers().get("Location").unwrap(), "/login");
-
-    // Part 3 - Reload the login page
+    
+    // Part 2 - Follow the redirect
     let response = app.get_login().await;
     let html_page = response.text().await.unwrap();
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
-
     
+    // Part 3 - Reload the login page
+    let response = app.get_login().await;
+    let html_page = response.text().await.unwrap();
+    assert!(!html_page.contains("<p><i>Authentication failed</i></p>"));
 }
 
