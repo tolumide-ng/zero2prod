36d91fcd59cbde45fb643c606840e95e901656b4
tolumide-ng <tolumideshopein@gmail.com>
2022-01-09 01:06:02 +0100

    Updates login route handler

--- src/routes/auth/login.rs
+++ src/routes/auth/login.rs
@@ -1,13 +1,19 @@
 use actix_web::{HttpResponse, web};
 use actix_web::http::header::{ContentType, LOCATION};
 use secrecy::Secret;
+use serde::Deserialize;
+use sqlx::PgPool;
+
+
+use crate::helpers::auth::{Credentials, validate_credentials};
 
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    username: String,
-    // password: Secret<String>
+    pub username: String,
+    pub password: Secret<String>
 }
+
 
 pub async fn login_form() -> HttpResponse {
     HttpResponse::Ok()
@@ -16,9 +22,27 @@ pub async fn login_form() -> HttpResponse {
 }
 
 
+#[tracing::instrument(
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+    tracing::Span::current()
+        .record("username", &tracing::field::display(&credentials.username));
 
-pub async fn login(_from: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/"))
-        .finish()
+    match validate_credentials(credentials, &pool).await {
+        Ok(user_id) => {
+            tracing::Span::current().record("user_id", &traicng::field::display(&user_id));
+            HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/"))
+                .finish()
+        }
+        Err(_) => {
+            todo!()
+        }
+    }
 }
