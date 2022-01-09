ac689603d234801a0378908ed52553b4d4280dfa
tolumide-ng <tolumideshopein@gmail.com>
2022-01-09 14:54:17 +0100

    Tests redirects after user login failure

--- tests/api/helpers/app.rs
+++ tests/api/helpers/app.rs
@@ -80,6 +80,19 @@ impl TestApp {
             .await
             .expect("Failed to execute request.")
     }
+
+    pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
+    where Body: serde::Deserialize,
+    {
+        reqwest::Client::builder()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap()
+            .post(&format!("{}/login", &self.address))
+            .form(body)
+            .send()
+            .await.expect("Failed to execute request.")
+    }
 }
 
 
