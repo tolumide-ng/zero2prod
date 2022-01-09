162104b1a76cc2ca72b96e488b144140dc3f5bd4
tolumide-ng <tolumideshopein@gmail.com>
2022-01-09 15:23:16 +0100

    Use the cookies api from rreqwest to get the cookies

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::helpers::app::spawn_app;
 
 #[tokio::test]
@@ -10,6 +12,11 @@ async fn an_error_flash_message_is_set_on_failure() {
     });
     let response = app.post_login(&login_body).await;
 
+    let flash_cookies = response.cookies().find(|c| c.name() == "_flash").unwrap();
+
+    assert_eq!(flash_cookies.valu(), "Authentication failed");
+
     assert_eq!(response.status().as_u16(), 303);
     assert_eq!(response.headers().get("Location").unwrap(), "/login");
 }
+
