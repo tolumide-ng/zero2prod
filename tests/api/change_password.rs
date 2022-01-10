f80ea09157c4d69a69564583ee29ec76e95604cf
tolumide-ng <tolumideshopein@gmail.com>
2022-01-10 22:50:36 +0100

    Tests that current password must be valid to change user's password

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -55,3 +55,31 @@ async fn new_password_fields_must_match() {
         the field values must match.</i></p>"
     ));
 }
+
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_password = Uuid::new_v4().to_string();
+
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    })).await;
+
+    let response = app.post_change_password(&serde_json::json!({
+        "current_password": &wrong_password,
+        "new_password": &new_password,
+        "new_password_check": &new_password
+    })).await;
+
+    assert_eq!(resposne.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), "/admin/password");
+
+    let response = app.get_change_password().await;
+    let html_page = response.text().await.unwrap();
+    assert!(html_page.contains(
+        "<p><i>The current password is incorrect.</i></p>"
+    ));
+}
