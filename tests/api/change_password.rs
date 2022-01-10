aaa9a369cc244829c001e646d57f732e0e49396b
tolumide-ng <tolumideshopein@gmail.com>
2022-01-10 22:34:26 +0100

    Adds test to confirm new_password and new_password_check similiarity

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -25,3 +25,33 @@ async fn you_must_be_logged_in_to_change_password() {
     assert_eq!(response.status().as_u16(), 303);
     assert_eq!(response.headers().get("Location").unwrap(), "/login");
 }
+
+
+#[tokio::test]
+async fn new_password_fields_must_match() {
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let another_new_password = Uuid::new_v4().to_string();
+
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    })).await;
+
+    let response = app.post_change_password(&serde_json::json!({
+        "current_password": &app.test_user.password,
+        "new_password": &new_password,
+        "new_password_check": &another_new_password
+    })).await;
+
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), "/admin/password");
+
+
+    let response = app.get_change_password().await;
+    let html_page = response.text().await.unwrap();
+    assert!(html_page.contains(
+        "<p><i>You entered two different new passwords - \
+        the field values must match.</i></p>"
+    ));
+}
